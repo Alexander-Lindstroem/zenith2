8856ff44f5ced5da47c81906128e6e34aa414443
May Sunktong <pattaranan.sunktong@gmail.com>
2025-02-12 11:11:25 +0100

    Starter for mammalRoute

--- routes/mammalRouter.js
+++ routes/mammalRouter.js
@@ -1,5 +1,25 @@
-import express from "express"
+import express from "express";
 
-const mammalRouter = express.Router()
+const mammalRouter = express.Router();
 
-export default mammalRouter
+mammalRouter.get("/", (req, res) => {
+  res.render("pages/welcome", {
+    pageType: "mammals",
+    headTitle: "Mammals",
+    title: "Mammals",
+    subtitle: "Our mammals",
+    bodyClass: "mammals-body",
+  });
+});
+
+// mammalRouter.get("/:mammalName", (req, res) => {
+//   const { mammalName } = req.params;
+//   const mammalData = mammals[mammalName];
+
+//   res.render("/animal", {
+//     ...baseData,
+//     mammalData: mammalData,
+//   });
+// });
+
+export default mammalRouter;
