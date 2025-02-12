75db90213316ec18dec7587c49667e9903c23593
May Sunktong <pattaranan.sunktong@gmail.com>
2025-02-12 12:17:51 +0100

    Refactor routing to be reusable, create welcome.ejs and 404.ejs

--- routes/mammalRouter.js
+++ routes/mammalRouter.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import { mammals } from '../data/animalData.js';
 const mammalRouter = express.Router();
 
 mammalRouter.get("/", (req, res) => {
@@ -12,35 +12,61 @@ mammalRouter.get("/", (req, res) => {
   });
 });
 
-mammalRouter.get("/echidna", (req, res) => {
-  res.render("pages/animal", {
-    pageType: "mammals",
-    headTitle: "Mammals",
-    title: "echidna",
-    subtitle: "Our mammals",
-    bodyClass: "mammals-body",
-  });
+// reusable routing for each mammal
+mammalRouter.get("/:mammalUrl", (req, res) => {
+  const mammalUrl = req.params.mammalUrl;
+  const animal = mammals.find(
+    (item) => item.name.toLowerCase() === mammalUrl.toLowerCase()
+  );
+
+  if (animal) {
+    res.render("pages/animal", {
+      pageType: "mammals",
+      headTitle: "Mammals",
+      title: animal.name,
+      subtitle: animal.description,
+      image: `/assets/images/${animal.imageFilename}`,
+      bodyClass: "mammals-body",
+    });
+  } else {
+    res.status(404).render("pages/404", {
+      headTitle: "Animal Not Found",
+      title: "Animal Not Found",
+      subtitle: "The requested animal does not exist.",
+      bodyClass: "not-found-body",
+    });
+  }
 });
 
-mammalRouter.get("/tasmanian-evil", (req, res) => {
-  res.render("pages/animal", {
-    pageType: "mammals",
-    headTitle: "Mammals",
-    title: "Tasmanian Devil",
-    subtitle: "Our mammals",
-    bodyClass: "mammals-body",
-  });
-});
+// mammalRouter.get("/echidna", (req, res) => {
+//   res.render("pages/animal", {
+//     pageType: "mammals",
+//     headTitle: "Mammals",
+//     title: "echidna",
+//     subtitle: "Our mammals",
+//     bodyClass: "mammals-body",
+//   });
+// });
 
-mammalRouter.get("/quokka", (req, res) => {
-  res.render("pages/animal", {
-    pageType: "mammals",
-    headTitle: "Mammals",
-    title: "quokka",
-    subtitle: "Our mammals",
-    bodyClass: "mammals-body",
-  });
-});
+// mammalRouter.get("/tasmanian-evil", (req, res) => {
+//   res.render("pages/animal", {
+//     pageType: "mammals",
+//     headTitle: "Mammals",
+//     title: "Tasmanian Devil",
+//     subtitle: "Our mammals",
+//     bodyClass: "mammals-body",
+//   });
+// });
+
+// mammalRouter.get("/quokka", (req, res) => {
+//   res.render("pages/animal", {
+//     pageType: "mammals",
+//     headTitle: "Mammals",
+//     title: "quokka",
+//     subtitle: "Our mammals",
+//     bodyClass: "mammals-body",
+//   });
+// });
 
 
 export default mammalRouter;
