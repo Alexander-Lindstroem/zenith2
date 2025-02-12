691a0f8e2cfdac5bceb3c45b95388ce00468a2a7
May Sunktong <pattaranan.sunktong@gmail.com>
2025-02-12 14:08:42 +0100

    Delete comments on mammalRoute

--- routes/mammalRouter.js
+++ routes/mammalRouter.js
@@ -40,35 +40,4 @@ mammalRouter.get("/:mammalUrl", (req, res) => {
   }
 });
 
-// mammalRouter.get("/echidna", (req, res) => {
-//   res.render("pages/animal", {
-//     pageType: "mammals",
-//     headTitle: "Mammals",
-//     title: "echidna",
-//     subtitle: "Our mammals",
-//     bodyClass: "mammals-body",
-//   });
-// });
-
-// mammalRouter.get("/tasmanian-evil", (req, res) => {
-//   res.render("pages/animal", {
-//     pageType: "mammals",
-//     headTitle: "Mammals",
-//     title: "Tasmanian Devil",
-//     subtitle: "Our mammals",
-//     bodyClass: "mammals-body",
-//   });
-// });
-
-// mammalRouter.get("/quokka", (req, res) => {
-//   res.render("pages/animal", {
-//     pageType: "mammals",
-//     headTitle: "Mammals",
-//     title: "quokka",
-//     subtitle: "Our mammals",
-//     bodyClass: "mammals-body",
-//   });
-// });
-
-
 export default mammalRouter;
