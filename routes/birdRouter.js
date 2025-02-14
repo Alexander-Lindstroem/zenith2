11bbcf2119ca3205ad4edd88322e07a79e001af4
Yevhenii <ke1vin.kapustian@gmail.com>
2025-02-14 14:36:46 +0100

    Changed names for variables

--- routes/birdRouter.js
+++ routes/birdRouter.js
@@ -7,7 +7,10 @@ birdRouter.get("/", (req, res) => {
     res.render("pages/animal", {
         headTitle: "Birds",
         headerTitle: "Birds",
-        welcomeMsg: "We are excited to invite you to explore our wonderful collection of birds, featuring the striking Cassowary, the cheerful Kookaburra, and the majestic Yellow-Tailed Black Cockatoo. Immerse yourself in the fascinating world of these incredible feathered creatures and learn about their unique traits, natural habitats, and behaviors. Enjoy your visit, and feel free to reach out if you have any questions or would like more information about our avian friends!"
+        welcomeMsgTitle: `Welcome to the birds page!`,
+        welcomeMsg: "We are excited to invite you to explore our wonderful collection of birds, featuring the striking Cassowary, the cheerful Kookaburra, and the majestic Yellow-Tailed Black Cockatoo. Immerse yourself in the fascinating world of these incredible feathered creatures and learn about their unique traits, natural habitats, and behaviors. Enjoy your visit, and feel free to reach out if you have any questions or would like more information about our avian friends!",
+        animalArray: birds,
+        pageType: "birds"
     }) 
 })
 
@@ -15,47 +18,56 @@ birdRouter.get("/cassowary", (req, res) => {
     let cassowary = birds[0];
     res.render("pages/animal", {
         headTitle: cassowary.name,
+        headerTitle: "Birds",
         infoImage: cassowary.imageFilename,
         birds: cassowary.name,
         infoLifespan: cassowary.lifespan,
         infoGroup: cassowary.group,
         infoFood: cassowary.food,
-        infoDescription: cassowary.description,
+        infoDesc: cassowary.description,
         infoLength: cassowary.length,
         infoWeight: cassowary.weight,
-        infoHabitat: cassowary.habitat
+        infoHabitat: cassowary.habitat,
+        animalArray: birds,
+        pageType: "birds"
     })
 })
 
 birdRouter.get("/kookaburra", (req, res) => {
-    let cassowary = birds[1];
+    let kookaburra = birds[1];
     res.render("pages/animal", {
-        headTitle: cassowary.name,
-        infoImage: cassowary.imageFilename,
-        birds: cassowary.name,
-        infoLifespan: cassowary.lifespan,
-        infoGroup: cassowary.group,
-        infoFood: cassowary.food,
-        infoDescription: cassowary.description,
-        infoLength: cassowary.length,
-        infoWeight: cassowary.weight,
-        infoHabitat: cassowary.habitat
+        headTitle: kookaburra.name,
+        headerTitle: "Birds",
+        infoImage: kookaburra.imageFilename,
+        birds: kookaburra.name,
+        infoLifespan: kookaburra.lifespan,
+        infoGroup: kookaburra.group,
+        infoFood: kookaburra.food,
+        infoDesc: kookaburra.description,
+        infoLength: kookaburra.length,
+        infoWeight: kookaburra.weight,
+        infoHabitat: kookaburra.habitat,
+        animalArray: birds,
+        pageType: "birds"
     })
 })
 
-birdRouter.get("/yellow-tailed-black-cockatoo", (req, res) => {
-    let cassowary = birds[2];
+birdRouter.get("/Yellow-Tailed%20Black%20Cockatoo", (req, res) => {
+    let yellowtailedblackcockatoo = birds[2];
     res.render("pages/animal", {
-        headTitle: cassowary.name,
-        infoImage: cassowary.imageFilename,
-        birds: cassowary.name,
-        infoLifespan: cassowary.lifespan,
-        infoGroup: cassowary.group,
-        infoFood: cassowary.food,
-        infoDescription: cassowary.description,
-        infoLength: cassowary.length,
-        infoWeight: cassowary.weight,
-        infoHabitat: cassowary.habitat
+        headTitle: yellowtailedblackcockatoo.name,
+        headerTitle: "Birds",
+        infoImage: yellowtailedblackcockatoo.imageFilename,
+        birds: yellowtailedblackcockatoo.name,
+        infoLifespan: yellowtailedblackcockatoo.lifespan,
+        infoGroup: yellowtailedblackcockatoo.group,
+        infoFood: yellowtailedblackcockatoo.food,
+        infoDesc: yellowtailedblackcockatoo.description,
+        infoLength: yellowtailedblackcockatoo.length,
+        infoWeight: yellowtailedblackcockatoo.weight,
+        infoHabitat: yellowtailedblackcockatoo.habitat,
+        animalArray: birds,
+        pageType: "birds"
     })
 })
 
