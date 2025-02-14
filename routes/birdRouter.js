0090f0c57b996cdf4e32194cd0caa421caeaf2ee
May Pattaranan <56375291+maysunktong@users.noreply.github.com>
2025-02-14 14:07:25 +0100

    Hot fix: add missing properties to bird and reptile

--- routes/birdRouter.js
+++ routes/birdRouter.js
@@ -8,7 +8,9 @@ birdRouter.get("/", (req, res) => {
         headTitle: "Birds",
         headerTitle: "Birds",
         welcomeMsgTitle: `Welcome to the birds page!`,
-        welcomeMsg: "We are excited to invite you to explore our wonderful collection of birds, featuring the striking Cassowary, the cheerful Kookaburra, and the majestic Yellow-Tailed Black Cockatoo. Immerse yourself in the fascinating world of these incredible feathered creatures and learn about their unique traits, natural habitats, and behaviors. Enjoy your visit, and feel free to reach out if you have any questions or would like more information about our avian friends!"
+        welcomeMsg: "We are excited to invite you to explore our wonderful collection of birds, featuring the striking Cassowary, the cheerful Kookaburra, and the majestic Yellow-Tailed Black Cockatoo. Immerse yourself in the fascinating world of these incredible feathered creatures and learn about their unique traits, natural habitats, and behaviors. Enjoy your visit, and feel free to reach out if you have any questions or would like more information about our avian friends!",
+        animalArray: birds,
+        pageType: "birds"
     }) 
 })
 
